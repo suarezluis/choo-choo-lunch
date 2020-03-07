f758a64353d1551a1a8ec1b5f9a087c45caebc68
Luis Suarez <suarezluis@gmail.com>
2020-03-07 15:54:31 -0600

    Updates

---
+++ precache-manifest.fd10349aaaebe82431ecf0988b903ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b1091c9120e4924927a0c32b93334e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fbd671d08d673c18b74",
+    "url": "./static/css/main.19e34edc.chunk.css"
+  },
+  {
+    "revision": "5f63348a05fa493a4d93",
+    "url": "./static/js/2.72f80ad3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.72f80ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fbd671d08d673c18b74",
+    "url": "./static/js/main.3a4e6d2e.chunk.js"
+  },
+  {
+    "revision": "82659c83a89fbfdc33fc",
+    "url": "./static/js/runtime-main.b9541938.js"
+  }
+]);

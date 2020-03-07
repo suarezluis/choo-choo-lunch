c45a86bb77a2939d4c2f3be5926fe0f14b593e9a
Luis Suarez <suarezluis@gmail.com>
2020-03-07 15:55:34 -0600

    Add deploy script

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+
 import data from "./data/index.js";
 export default class App extends Component {
-  state = { data, options: data.length, result: "", delay: 10 };
+  state = {
+    data,
+    options: data.length,
+    result: "",
+    delay: 10
+  };
   getRandom = () => {
     let isNew = false;
     while (isNew === false) {
       let index = Math.floor(Math.random() * this.state.options);
       if (this.state.result.name !== this.state.data[index].name) {
-        this.setState({ result: data[index], delay: this.state.delay + 10 });
+        this.setState({
+          result: data[index],
+          delay: this.state.delay + 10
+        });
         isNew = true;
       }
     }
@@ -26,21 +35,36 @@ export default class App extends Component {
     return (
       <div
         style={{
-          // width: "100%",
-          // height: "100%",
-          // flexDirection: "column",
           display: "flex",
           flex: 1,
-          // width: "100%",
+          flexDirection: "column",
           alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "green"
+          justifyContent: "center"
         }}
       >
         <img
-          src={this.state.result.image}
-          style={{ width: "25%", backgroundColor: "white", borderRadius: 50 }}
+          style={{ height: "15%", width: "auto" }}
+          src="https://www.veterinarian-hospital.com/wp-content/uploads/2019/03/televet-logo-300x158.png"
         />
+        <h1>Lunch choo choo train</h1>
+        <h2>All aboard!</h2>
+        <img
+          src={this.state.result.image}
+          style={{
+            height: "15%",
+            width: "auto",
+            padding: "5%",
+            backgroundColor: "white",
+            borderRadius: 50
+          }}
+        />
+        <br />
+        {this.state.delay >= 10 ? (
+          <img
+            style={{ height: "15%", width: "auto" }}
+            src="https://chew-chewtrain.com/wp-content/uploads/2015/11/trainred.gif"
+          />
+        ) : null}
       </div>
     );
   }
--- src/data/index.js
+++ src/data/index.js
@@ -1,26 +1,32 @@
 export default [
   {
-    name: "Chi-Lantro",
-    website: "www,chilantro.com",
+    name: "Chi'Lantro",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQSPQXYi-WNB789-OcSUpCRtLo721McbT1pXTSaoSD8iVRxj_Pr"
   },
   {
     name: "Cava",
-    website: "www.cava.com",
     image:
       "https://borntocoupon.com/wp-content/uploads/2018/09/Cava-Mezze-Grill-Logo.png"
   },
   {
-    name: "Cuatro Gatti",
-    website: "www.cava.com",
+    name: "Quattro Gatti",
     image:
       "http://static1.squarespace.com/static/5529788ce4b05e87433cc7cd/t/552dc8c6e4b05ea8e143da9b/1582221137109/?format=1500w"
   },
   {
     name: "Jimmy Johns",
-    website: "www.cava.com",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Jimmy_Johns_logo.svg/512px-Jimmy_Johns_logo.svg.png"
+  },
+  {
+    name: "P Terry's",
+    image:
+      "https://static.spacecrafted.com/b55ed33c13d643ed93f54b2b9e66400f/i/b796b3962f5149aab53d330de9a8b8f3/1/5feFb8zhrk/logoRegular.png"
+  },
+  {
+    name: "TacoDeli",
+    image:
+      "https://www.winningcause.org/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/t/a/tacodelilogo_current.jpg"
   }
 ];
